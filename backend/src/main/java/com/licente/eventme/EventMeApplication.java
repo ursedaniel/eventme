3895d2029925bc46187b2771f05958f49656bc88
daniel_urse <daniel.urse@hotmail.com>
2018-06-07 17:35:48 +0300

    -backend first commit

---
+++ backend/src/main/java/com/licente/eventme/EventMeApplication.java
@@ -0,0 +1,12 @@
+package com.licente.eventme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventMeApplication.class, args);
+	}
+}
---
+++ backend/src/test/java/com/licente/eventme/EventMeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.licente.eventme;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EventMeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
